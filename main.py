e9b23a6a3d64b442f5b6121d01269dec2efb7cae
branislavblazek <branislav.blazek.bb@gmail.com>
2020-01-17 17:07:28 +0100

    funguje prepinanie

--- main.py
+++ main.py
@@ -4,26 +4,24 @@ import sys
 import random
 from krokovanie.zajo_main import zajo_main_func
 from zabky.zabky_main import zabky_main_func
+from files.window import Consts_window
+from files.colors import Consts_colors
 
 pygame.init()
 
-display_width = 800
-display_height = 600
+const = {
+    'window': Consts_window(),
+    'colors': Consts_colors()
+}
 
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-
-screen = pygame.display.set_mode((display_width, display_height))
+screen = pygame.display.set_mode((const['window'].WIDTH, const['window'].HEIGHT))
 pygame.display.set_caption('ahoj svet')
 
 def game_intro():
     text_one = pygame.font.Font('freesansbold.ttf', 115)
 
-    surface_one = text_one.render('prva_hra', True, green)
-    surface_two = text_one.render('druha_hra', True, red)
+    surface_one = text_one.render('prva_hra', True, const['colors'].BLUE)
+    surface_two = text_one.render('druha_hra', True, const['colors'].GREEN)
 
     text_obj_one = surface_one.get_rect()
     text_obj_one.center = (400, 150)
@@ -32,18 +30,23 @@ def game_intro():
     text_obj_two.center = (400, 450)
 
     while True:
+        #------MAIN LOOP
         for event in pygame.event.get():
+            #------QUIT EVENT
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            #-----CLICK EVENT
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 coors = event.pos
                 if text_obj_one.collidepoint(coors):
+                    #-----START ZAJO
                     zajo_main_func(pygame, screen)
                 elif text_obj_two.collidepoint(coors):
+                    #-----START ZABKY
                     zabky_main_func(pygame, screen)
 
-        screen.fill(white)
+        screen.fill(const['colors'].WHITE)
         screen.blit(surface_one, text_obj_one)
         screen.blit(surface_two, text_obj_two)
         pygame.display.update()
