fdd3abf06aa39b35a0cf0535caefacfb3df7aeaf
branislavblazek <branislav.blazek.bb@gmail.com>
2020-02-05 00:00:56 +0100

    rozsirenie zabky

--- zabky/main_obj.py
+++ zabky/main_obj.py
@@ -10,10 +10,86 @@ class Level:
         images = {
             "farm": self.pg.image.load(path + 'farm.png'),
             "log_end": self.pg.image.load(path + 'log_end.png'),
-            "log": self.pg.image.load(path + 'log.png')
+            "log": self.pg.image.load(path + 'log.png'),
+            "box": self.pg.image.load(path + 'box.png'),
+            "weight": self.pg.image.load(path + 'weight.png')
         }
         images['farm'] = self.pg.transform.scale(images['farm'], (window_width, window_height))
-        images['log_end'] = self.pg.transform.scale(images['log_end'], (150, 150))
-        images['log'] = self.pg.transform.scale(images['log'], (320,160))
+        images['log_end'] = self.pg.transform.scale(images['log_end'], (200, 200))
+        images['log'] = self.pg.transform.scale(images['log'], (520,200))
+        images['box'] = self.pg.transform.scale(images['box'], (360, 360))
+        images['weight'] = self.pg.transform.scale(images['weight'], (180, 200))
+
+        #adding text on weight img
+        sprite = self.pg.sprite.Sprite()
+        sprite.image = images['weight']
+        sprite.rect = images['weight'].get_rect()
+        sprite.rect.topleft = (65, 300)
+        font = self.pg.font.SysFont('Sans', 50)
+        text = font.render('21', True, (0,0,0))
+        xxx = self.pg.Rect(65,80,50,50)
+        sprite.image.blit(text, xxx)
+        group = self.pg.sprite.Group()
+        group.add(sprite)
+        images['weight'] = group
 
         return images
+
+class Zabka:
+    def __init__(self, vaha, pg):
+        self.pg = pg
+        self.vaha = vaha
+        self.index = -1
+        self.base_coor = [620, 300]
+        self.x = self.base_coor[0]
+        self.y = self.base_coor[1]
+        self.jump_base = 11
+        self.jump_count = self.jump_base
+        self.is_jumping = False
+        #-1 is left, 1 is right, -1 is default
+        self.jump_direction = -1
+        self.na_hojdacke = 0
+        path = 'zabky/resources/'
+        self.img = self.pg.image.load(path + 'frog.png')
+        self.img = self.pg.transform.scale(self.img, (120, 150))
+
+        sprite = self.pg.sprite.Sprite()
+        sprite.image = self.img
+        sprite.rect = self.img.get_rect()
+        sprite.rect.x = self.x
+        sprite.rect.y = self.y
+        font = self.pg.font.SysFont('Sans', 40)
+        text = font.render(str(self.vaha), True, (0,0,0))
+        xxx = self.pg.Rect(40,80,50,50)
+        sprite.image.blit(text, xxx)
+        group = self.pg.sprite.Group()
+        group.add(sprite)
+        self.img = group
+
+    @property
+    def kresli_info(self):
+        if self.is_jumping:
+            if self.jump_count >= -self.jump_base:
+                neg = 1
+                if self.jump_count < 0:
+                    neg = -1
+                self.y -= (self.jump_count ** 2) * 0.5 * neg
+                self.x += 13 * self.jump_direction
+                self.jump_count -= 1
+            else:
+                self.is_jumping = False
+                self.jump_count = self.jump_base
+                if self.jump_direction == 1:
+                    self.na_hojdacke = 0
+                    self.jump_direction = -1
+                elif self.jump_direction == -1:
+                    self.na_hojdacke = self.vaha
+                    self.jump_direction = 1
+        self.img.sprites()[self.index].rect.left = self.x
+        self.img.sprites()[self.index].rect.top = self.y
+        return self.img
+
+    def as_rect(self, index):
+        self.index = index
+        copy_img = self.img.sprites()[index].rect
+        return copy_img
--- zabky/zabky_game.py
+++ zabky/zabky_game.py
@@ -2,7 +2,7 @@
 from pygame.locals import *
 import sys
 
-from zabky.main_obj import Level
+from zabky.main_obj import Level, Zabka
 
 #import from sibling folder
 sys.path.insert(0, "..")
@@ -15,23 +15,48 @@ const['color'] = files.colors.Consts_colors()
 const['window'] = files.window.Consts_window()
 
 def zabky_level(pg, screen, level_status):
+    mouse_clicked = False
+    mouse_coor = (0,0)
     #init the main object
     level = Level(level_status, pg)
     #get images
     images = level.load_images(const['window'].WIDTH, const['window'].HEIGHT)
+    #zabky
+    frogs = []
+    frogs.append(Zabka(15, pg))
     #------------MAIN LOOP
     while True:
         #---------PLACE IMAGES
         #set backgrund image
         screen.blit(images['farm'], (0,0))
         #set log end
-        screen.blit(images['log_end'], (const['window'].WIDTH//3-(150//2), 580))
+        screen.blit(images['log_end'], (const['window'].WIDTH//3-100, 500))
         #set log
-        screen.blit(images['log'], (const['window'].WIDTH//3-(150//2)-160, 420))
+        screen.blit(images['log'], (const['window'].WIDTH//3-270, 390))
+        #set box
+        screen.blit(images['box'], (610, 420))
+        #set weight
+        #screen.blit(images['weight'], (65,300))
+        images['weight'].draw(screen)
+        #vaha na hojdacke
+        spolu_na_hojdacke_vaha = 0
+        #set frog
+        for index, frog in enumerate(frogs):
+            if frog.as_rect(index).collidepoint(mouse_coor) and mouse_clicked:
+                mouse_clicked = False
+                frog.is_jumping = True
+            frog.kresli_info.draw(screen)
+            spolu_na_hojdacke_vaha += frog.na_hojdacke
+        print(spolu_na_hojdacke_vaha)
 
         #---------EVENTS
         for event in pg.event.get():
             if event.type == QUIT:
                 pq.quit()
                 sys.exit()
-        pg.display.flip()
+            elif event.type == MOUSEMOTION:
+                mouse_coor = event.pos
+            elif event.type == MOUSEBUTTONDOWN:
+                mouse_coor = event.pos
+                mouse_clicked = True
+        pg.display.update()
